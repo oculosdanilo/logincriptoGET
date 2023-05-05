63bce1a9373e287a264515c61b48f1f39ef1dd4e
Your Name <you@example.com>
2023-05-05 15:24:53 -0300

    05/05/2023 15:24:53

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Homepage</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-32x32.png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="main">
+      <h1>Olá, <?=$_COOKIE['username']?>!</h1>
+      <?php if ($_COOKIE['admin'] == 1): ?>
+        <p>Você é um administrador</p>
+      <?php else: ?>
+        <p>Você não é um administrador</p>
+      <?php endif; ?>
+      <p>Token de acesso: <?=$_COOKIE["token"]?></p>
+    </div>
+  </body>
+</html>
