639f0f2c8df386b5ef0ec6b77acb32f305ba10e4
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-05-06 15:44:01 -0300

    05/06/2023 15:44:01

--- GET/usuarios/index.php
+++ GET/usuarios/index.php
@@ -5,28 +5,37 @@ require "../../ler.php";
 $registro = json_decode(ler("../../senhas.txt")[0]);
 $achouUser = false;
 
-if (isset($_GET["id"])) {
-  for ($i=0; $i < count($registro); $i++) { 
-    if ($_GET["id"] == $registro[$i]->id) {
+if (isset($_GET["id"]))
+{
+  for ($i = 0; $i < count($registro); $i++)
+  {
+    if ($_GET["id"] == $registro[$i]->id)
+    {
       print_r(json_encode($registro[$i]));
       $achouUser = true;
     }
   }
   $achouUser ? print("") : print("Usuário não existe");
-} else {
+}
+else
+{
   print_r(json_encode($registro));
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
+  <title>Informações Login</title>
+  <link rel="stylesheet" href="../../main.css">
 </head>
+
 <body>
-  
+  <div class="main"></div>
 </body>
+
 </html>
